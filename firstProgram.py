aa286a34f1209e6ee45fc75607eb7f527a98a833
Nicolas Boulet <nicolasboulet@live.fr>
2021-09-23 19:06:59 +0200

    Fisrt Program

---
+++ firstProgram.py
@@ -0,0 +1,13 @@
+# a comment
+
+print('Hello World!')
+
+print('What is your name?') # another comment
+myName = input()
+print('Hello' + myName)
+print('The Length of your name is :')
+print(len(myName))
+
+print('What is your age ?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in one Year' )
